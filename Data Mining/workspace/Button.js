c90ec00dfc0e6ad9bdaabe9001d39eba88d19b04
Aditya Kulkarni <kulkarniaditya1997@gmail.com>
2023-09-02 18:58:15 -0700

    Gpt-engineer output and screencase

---
+++ Data Mining/workspace/Button.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Button({ onClick }) {
+  return <button onClick={onClick}>Get Weather</button>;
+}
+
+export default Button;
---
+++ Data Mining/workspace/utils.js
@@ -0,0 +1,3 @@
+function convertTemperature(celsius) {
+  return (celsius * 9/5) + 32;
+}
