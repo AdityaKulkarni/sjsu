17f22c04ccbcc10b0ab701d914debbe8d3e3a4c2
Aditya Kulkarni <kulkarniaditya1997@gmail.com>
2023-10-16 22:17:42 -0700

    Add files via upload

---
+++ Enterprise Software Platform/Assignments/springboot/src/test/java/com/sjsu/chatbackend2/ChatBackend2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sjsu.chatbackend2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatBackend2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
